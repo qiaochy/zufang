2455896c0566b46b905bfc31c60509e291dafef7
晓峰 <987368515@qq.com>
2016-11-04 15:05:33 +0800

    登录&&后台显示

--- yii2/backend/config/main.php
+++ yii2/backend/config/main.php
@@ -39,4 +39,5 @@ return [
         */
     ],
     'params' => $params,
+     'defaultRoute' => 'login/index'
 ];
--- yii2/common/config/main-local.php
+++ yii2/common/config/main-local.php
@@ -1,9 +1,10 @@
 <?php
 return [
+
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii',
+            'dsn' => 'mysql:host=192.168.1.30;dbname=yii',
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8',
@@ -14,7 +15,33 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [  
+                                    'class' => 'Swift_SmtpTransport',  
+                                    'host' => 'smtp.163.com',  //每种邮箱的host配置不一样
+                                    'username' => '18511694089@163.com',  
+                                    'password' => 'feng987368515',  
+                                    'port' => '25',  
+                                    'encryption' => 'tls',                             
+                                    ],   
+            'messageConfig'=>[  
+                                     'charset'=>'UTF-8',  
+                                     'from'=>['18511694089@163.com'=>'幻想科技']  
+                                     ],  
+        ],
+        'request'=>array(
+            // Enable Yii Validate CSRF Token
+            'enableCsrfValidation' => false,
+        ),
+        'assetManager'=>[
+            'bundles'=>[
+                'yii\web\JqueryAsset'=>[
+                    'jsOptions'=>[
+                        'position'=>\yii\web\View::POS_HEAD,
+                    ]
+                ]
+            ]
         ],
     ],
+
 ];
